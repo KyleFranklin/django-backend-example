76707fdf50af5218284c98d29a1d007c031bdc44
Kyle Franklin <kylefranklin554@gmail.com>
2024-07-16 15:46:31 -0400

    Add files via upload

--- backend/user_api/views.py
+++ backend/user_api/views.py
@@ -6,19 +6,22 @@ from .serializer import UserRegisterSerializer, UserLoginSerializer, UserSeriali
 from rest_framework import permissions, status
 from .validations import custom_validation
 from rest_framework.permissions import IsAuthenticated
-
+from django.core.exceptions import ValidationError
 
 class UserRegister(APIView):
-	permission_classes = (permissions.AllowAny,)
-	def post(self, request):
-		clean_data = custom_validation(request.data)
-		serializer = UserRegisterSerializer(data=clean_data)
-		if serializer.is_valid(raise_exception=True):
-			user = serializer.create(clean_data)
-			if user:
-				return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(status=status.HTTP_400_BAD_REQUEST)
+    permission_classes = (permissions.AllowAny,)
 
+    def post(self, request):
+        try:
+            clean_data = custom_validation(request.data)
+            serializer = UserRegisterSerializer(data=clean_data)
+            if serializer.is_valid(raise_exception=True):
+                user = serializer.create(clean_data)
+                if user:
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        except ValidationError as e:
+            return Response({'error': e.messages}, status=status.HTTP_400_BAD_REQUEST)
 
 class UserLogin(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -27,20 +30,19 @@ class UserLogin(APIView):
     def post(self, request):
         data = request.data
         serializer = UserLoginSerializer(data=data)
-        
+
         if serializer.is_valid(raise_exception=True):
             user = serializer.check_user(data)
             login(request, user)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-
 class UserLogout(APIView):
-	permission_classes = (permissions.AllowAny,)
-	authentication_classes = ()
-	def post(self, request):
-		logout(request)
-		return Response(status=status.HTTP_200_OK)
+    permission_classes = (permissions.AllowAny,)
+    authentication_classes = ()
 
+    def post(self, request):
+        logout(request)
+        return Response(status=status.HTTP_200_OK)
 
 class UserView(APIView):
     permission_classes = (IsAuthenticated,)
